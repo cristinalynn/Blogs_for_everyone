fd406e59a1e1cc823ec5d4d33733fcf000db0c54
Cristina Lynn <cristina@cristinalynn.net>
2024-03-13 13:00:49 -0700

    Project phase 3

---
+++ lib/db/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///info.db')
+Base = declarative_base()
+
+class Author(Base):
+    __tablename__ = 'authors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    blogs = relationship("Blog", back_populates='author')
+
+    def __repr__(self):
+        return f'<Author(id={self.id}, name={self.name})>'
+class Blog(Base):
+    __tablename__= 'blogs'
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String(), nullable=False)
+    author_id = Column(Integer, ForeignKey('author.id'))
+    author = relationship('Author', back_populates='blogs')
+
+    def __repr__(self):
+        return f'<Blog(id={self.id}, title={self.title}, authir_id={self.author_id})>'
+    
+class Reader(Base):
+    __tablename__ = 'readers'
+
+    id = Column(Integer(), primary_key= True)
+    name = Column(String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Reader(id={self.id}, name={self.name})>'
+    
+    Base.metadata.create_all(engine)
+
---
+++ lib/db/seed.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from faker import Faker
+from models import Author, Blog, Reader
+
+#Creates the engine
+engine = create_engine('sqlite://info.db')
+
+#Creates a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+#Faker
+faker = Faker()
+
+#Create instances of classes here...
+def seed_data():
+
+    # Authors
+    for _ in range(5):
+        author = Author(name=faker.name())
+        session.add(author)
+    session.commit()
+    print("Number of authors inserted:", session.query(Author).count())
+
+    # Blogs
+    authors = session.query(Author).all()
+    for _ in range(10):
+        blog = Blog(title=faker.catch_phrase(), author=faker.random_element(authors))
+        session.add(blog)
+    session.commit()
+    print("Number of blogs inserted:", session.query(Blog).count())
+
+    # Readers
+    for _ in range(10):
+        reader = Reader(name=faker.name(), year_joined=faker.year())
+        session.add(reader)
+    session.commit()
+    print("Number of readers inserted:", session.query(Reader).count())
+
+if __name__ == "__main__":
+    seed_data()
+    session.close()
